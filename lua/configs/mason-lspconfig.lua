67c1a58227bdc1e71a60666d79ce98dcd577d053
shayon07 <96220936+shayon07@users.noreply.github.com>
2026-02-20 00:18:47 +0600

    Update LSP servers list in mason-lspconfig
    Removed 'clangd' from the list of LSP servers to install.

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -14,7 +14,7 @@ local function table_contains(table, value)
 end
 
 -- Build a list of lsp servers to install minus the ignored list.
-local all_servers = { "clangd", "lua_ls" }
+local all_servers = { "lua_ls" }
 -- for _, s in ipairs(lspconfig.servers) do
 --     if not table_contains(ignore_install, s) then
 --         table.insert(all_servers, s)
